3156ad53f8ddd94d17485edfe1d3eaf0a3b64319
“Sharvari” <sharvari@lifeguarddh.com>
2023-08-08 14:23:23 -0700

    Initial Commit

---
+++ SimpleCICDDemo/SimpleCICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCICDDemoApp.swift
+//  SimpleCICDDemo
+//
+//  Created by Sharvari on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
